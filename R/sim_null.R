5209a34ca1504d977fbff351f69ea09cdc04ba7a
Chang8471 <54909676+Chang8471@users.noreply.github.com>
2022-10-03 23:23:55 -0400

    change parameter sim setup for both simData functions and update documentation

--- R/sim_null.R
+++ R/sim_null.R
@@ -11,7 +11,9 @@
 #' @param alwaysOnGenes for the endogenous probes, set certain amount always expressed in all samples, default = 0
 #' @param fixedBackground boolean indicator, if true, all samples will share the same background mean and variance parameters
 #' @param d0 prior degree of freedom for simulating signal variance
-#' @param alpha_Offset a numerical value to offset all alpha_i, the larger the further away all signals will be from the noise
+#' @param beta_offset a numerical value to offset all beta_offset, the larger the further away all signals will be from the noise
+#' @param seed.par seed for simulating parameters, default = NULL
+#' @param seed.data seed for simulating Z and Y, default = NULL
 #'
 #' @return a list of data and simulated true parameters
 #'
@@ -22,7 +24,7 @@
 #' \item `Pos_control`  a matrix of all observed counts for Positive control probes. Probes by sample
 #' \item `pi_i` vector of simulated sample marginal prior probabilty of observing signal
 #' \item `mu_0i` vector simulated background mean
-#' \item `sigma_0i` vector of simulate background variance
+#' \item `sigma_0i` vector of simulate background sd
 #' \item `alpha_i` vector of simulated sample contribution to signal mean, offset included
 #' \item `beta_g` vector of simulated probe contribution to signal mean
 #' \item `alpha_i_beta_g` matrix of signal mean, same dimensions as `Y_mtx`
@@ -34,19 +36,18 @@
 #' @examples
 simData = function(nEndogenous = 800, nPosControl = 10, nNegControl = 5,
                    nSample = 500, alwaysOnGenes = 0,
-                   fixedBackground = T, d0 = 50, alpha_Offset = 2){
+                   fixedBackground = T, d0 = 50, beta_offset = 0,
+                   seed.par=NULL, seed.data=NULL){
 
-  inv.logit = function(x){1/(exp(-x)+1)}
+
   nGene = nEndogenous+nPosControl+nNegControl
-  # background
-  mu_0i = rlnorm(nSample, 3,.1)
-  if (fixedBackground) mu_0i = rep(20,nSample)
-  sigma_0i = mu_0i*exp(-.9)
-  Y_background = matrix(rnorm(nSample*nGene,mu_0i,sigma_0i),nrow=nGene, byrow = T)
 
-  # signal
-  alpha_i = alpha_Offset+rlnorm(nSample,1,.1) # assume independence
-  beta_g = rnorm(nGene,3.4,2/3)
+
+  # parameters for signal component
+  if(seed.par) set.seed(seed.par)
+  alpha_i = rnorm(nSample,0,.1)
+  beta_g = rnorm(nGene,3.4,2/3)+ beta_offset+mean(alpha_i)
+  alpha_i = alpha_i-mean(alpha_i)
   beta_g[(nGene-nPosControl+1):nGene] = 1:nPosControl # pos control, gradient
   sigma2_10 = exp(beta_g/1.3-4)
   sigma2_1g = (d0*sigma2_10)/rchisq(nGene,d0)
@@ -54,9 +55,14 @@ simData = function(nEndogenous = 800, nPosControl = 10, nNegControl = 5,
   alphai_beta_g = matrix(0,nrow = nGene,ncol = nSample)
   alphai_beta_g = sweep(alphai_beta_g,1,beta_g,"+")
   alphai_beta_g = sweep(alphai_beta_g,2,alpha_i,"+")
-  Y_signal = matrix(rlnorm(nGene*nSample,c(alphai_beta_g),sqrt(sigma2_1g)),nrow = nGene)
 
-  # Z_gi
+  # background parameters
+  mu_0i = rlnorm(nSample, 2.43,.44)
+  if (fixedBackground) mu_0i = rep(20,nSample)
+  sigma_0i = rnorm(nSample, mu_0i*exp(-.9),.03*mu_0i)
+
+  # Z_gi, fix a proportion of genes expressed in each sample
+  if(seed.data) set.seed(seed.data)
   pi_i = inv.logit(rnorm(nSample,-1.3,.4))
   pi_i[pi_i<0] = 0; pi_i[pi_i>1] = 1
   Z_mtx = matrix(rbinom(nSample*nGene,1,pi_i), nrow = nGene,byrow = T)
@@ -64,6 +70,9 @@ simData = function(nEndogenous = 800, nPosControl = 10, nNegControl = 5,
   Z_mtx[(nGene-nPosControl+1):nGene,] =1 # positive control
   Z_mtx[(nGene-nPosControl-nNegControl+1):(nGene-nPosControl),] =0 # neg control
 
+  # simulate Y|Z
+  Y_signal = matrix(rlnorm(nGene*nSample,c(alphai_beta_g),sqrt(sigma2_1g)),nrow = nGene)
+  Y_background = matrix(rnorm(nSample*nGene,mu_0i,sigma_0i),nrow=nGene, byrow = T)
   Y_mtx_wCtrl = ifelse(Z_mtx, Y_signal+Y_background, Y_background)
   Y_mtx_wCtrl = ceiling(Y_mtx_wCtrl)
   Y_mtx_wCtrl[Y_mtx_wCtrl<1] = 1
@@ -97,12 +106,10 @@ simData = function(nEndogenous = 800, nPosControl = 10, nNegControl = 5,
 #' @param alwaysOnGenes for the endogenous probes, set certain amount always expressed in all samples, default = 0
 #' @param fixedBackground boolean indicator, if true, all samples will share the same background mean and variance parameters
 #' @param d0 prior degree of freedom for simulating signal variance
-#' @param alpha_Offset a numerical value to offset all alpha_i, the larger the further away all signals will be from the noise
-#' @param seed.ab seed for simulating parameters for signal components
-#' @param seed.sim seed for simulating background parameters, Z and Y
+#' @param beta_offset a numerical value to offset all beta_offset, the larger the further away all signals will be from the noise
+#' @param seed.par seed for simulating parameters, default = NULL
+#' @param seed.data seed for simulating Z and Y, default = NULL
 #' @param expressPercent fixed proportion of endogenous probes expressed in each sample
-#' @param alpha_shape1 first parameter to simulated alpha_i from rbeta()
-#' @param alpha_shape2 second parameter to simulated alpha_i from rbeta()
 #'
 #' @return a list of data and simulated true parameters
 #'
@@ -113,7 +120,7 @@ simData = function(nEndogenous = 800, nPosControl = 10, nNegControl = 5,
 #' \item `Pos_control`  a matrix of all observed counts for Positive control probes. Probes by sample
 #' \item `pi_i` true prior probability of signal in a sample, same as `expressPercent` input parameter
 #' \item `mu_0i` vector simulated background mean
-#' \item `sigma_0i` vector of simulate background variance
+#' \item `sigma_0i` vector of simulate background sd
 #' \item `alpha_i` vector of simulated sample contribution to signal mean, offset included
 #' \item `beta_g` vector of simulated probe contribution to signal mean
 #' \item `alpha_i_beta_g` matrix of signal mean, same dimensions as `Y_mtx`
@@ -126,19 +133,18 @@ simData = function(nEndogenous = 800, nPosControl = 10, nNegControl = 5,
 simData_fixSigProp = function(nEndogenous = 800,
                               nPosControl = 10, nNegControl = 10,
                               nSample = 500, alwaysOnGenes = 0,
-                              fixedBackground = F, d0 = 50, alpha_Offset = 0,
-                              seed.ab = 0,seed.sim = 1, expressPercent = 1/5,
-                              alpha_shape1=3,alpha_shape2 =3){
-
+                              fixedBackground = F, d0 = 50, expressPercent = 1/5,beta_offset = 0,
+                              seed.par=NULL, seed.data=NULL){
 
 
   nGene = nEndogenous+nPosControl+nNegControl
 
 
   # parameters for signal component
-  set.seed(seed.ab)
-  alpha_i = rbeta(nSample,alpha_shape1,alpha_shape2) +alpha_Offset
-  beta_g = rnorm(nGene,3.4,2/3)
+  if(seed.par) set.seed(seed.par)
+  alpha_i = rnorm(nSample,0,.1)
+  beta_g = rnorm(nGene,3.4,2/3)+ beta_offset+mean(alpha_i)
+  alpha_i = alpha_i-mean(alpha_i)
   beta_g[(nGene-nPosControl+1):nGene] = 1:nPosControl # pos control, gradient
   sigma2_10 = exp(beta_g/1.3-4)
   sigma2_1g = (d0*sigma2_10)/rchisq(nGene,d0)
@@ -147,15 +153,13 @@ simData_fixSigProp = function(nEndogenous = 800,
   alphai_beta_g = sweep(alphai_beta_g,1,beta_g,"+")
   alphai_beta_g = sweep(alphai_beta_g,2,alpha_i,"+")
 
-
-  set.seed(seed.sim)
   # background parameters
-  mu_0i = rlnorm(nSample, 3,.1)
+  mu_0i = rlnorm(nSample, 2.43,.44)
   if (fixedBackground) mu_0i = rep(20,nSample)
-  sigma_0i = mu_0i*exp(-.9)
-
+  sigma_0i = rnorm(nSample, mu_0i*exp(-.9),.03*mu_0i)
 
   # Z_gi, fix a proportion of genes expressed in each sample
+  if(seed.data) set.seed(seed.data)
   Z_mtx = matrix(0,nGene,nSample)
   tmp = rep(0,nEndogenous-alwaysOnGenes)
   tmp[1:floor(expressPercent*(nEndogenous-alwaysOnGenes))]=1
@@ -187,4 +191,3 @@ simData_fixSigProp = function(nEndogenous = 800,
     sigma2_10 = sigma2_10[1:nEndogenous], sigma2_1g = sigma2_1g[1:nEndogenous]
   ))
 }
-
